f8bfb08c80f66d23fe28b4ccfe472f2db78cbc8a
Devorah Stucki <61331916+devorahst@users.noreply.github.com>
2022-08-11 15:18:23 -0600

    fix typo

--- formating_scotts_files.R
+++ formating_scotts_files.R
@@ -7,7 +7,6 @@ setwd("/Users/devorahstucki/Downloads/scripts/eGFR-Visualization-Tool")
 
 master.file <- read_tsv("egfr_table_files/slope_cohort_summary_right_censored_vitals_labs_meds_insulin.tsv") 
 
-master.file %>% 
-  select(dist_id, sex, ethn, age_first_egfr, first_egfr, first_ckd_stage, age_last_egfr, last_egfr, last_ckd_stage, n_egfr, followup_yrs, corrected_slope, corrected_r2, obsv_esrd_fu_yrs, icd_esrd_fu_yrs, icd_dialysis_fu_yrs, icd_kidney_tx_fu_yrs, uncorrected_slope)
+masterfile = select(master.file, c(dist_id, sex, ethn, age_first_egfr, first_egfr, first_ckd_stage, age_last_egfr, last_egfr, last_ckd_stage, n_egfr, followup_yrs, corrected_slope, corrected_r2, obsv_esrd_fu_yrs, icd_esrd_fu_yrs, icd_dialysis_fu_yrs, icd_kidney_tx_fu_yrs, uncorrected_slope))
 
-write.table(master.file, file = "masterfile.tsv", sep = "\t", row.names = FALSE)
+write.table(masterfile, file = "masterfile.tsv", sep = "\t", row.names = FALSE)
